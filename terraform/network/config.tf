7d3c9f41e5ef4987fe4d411af57cd1b76e7e1147
suraj447 <ec2-user@ip-172-31-22-227.ec2.internal>
2024-08-17 21:39:11 +0000

    change bucket name

--- terraform/network/config.tf
+++ terraform/network/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "acs730-final-group9-bucket"
+    bucket = "acs-730-final-group9-bucket"
     key    = "prod/network/terraform.tfstate"
     region = "us-east-1"
   }
